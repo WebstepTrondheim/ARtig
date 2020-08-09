43757114c5ce5f8fcab3c91d71fc1e40b8c35f65
Pål Christian Hagen <pal.hagen@webstep.no>
2020-08-09 14:51:43 +0200

    Playing sound when the ball hits any part of the bucket

---
+++ Assets/ARtig/Scripts/BallBucketCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BallBucketCollision : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource _sound;
+
+    void OnCollisionEnter()
+    {
+        _sound.Play();
+    }
+}

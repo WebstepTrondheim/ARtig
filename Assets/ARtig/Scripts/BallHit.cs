ed9eb71b398e1c40e2d597ff501d78155b90e43a
Pål Christian Hagen <pal.hagen@webstep.no>
2020-08-09 15:39:41 +0200

    Using BillBoard component to make sure the status text is always facing the player. Displaying score.

--- Assets/ARtig/Scripts/BallHit.cs
+++ Assets/ARtig/Scripts/BallHit.cs
@@ -4,25 +4,12 @@ using UnityEngine;
 
 public class BallHit : MonoBehaviour
 {
-    //public GameObject score; //reference to the ScoreText gameobject, set in editor
-    //public AudioClip basket; //reference to the basket sound
-
     public TextMesh score;
     private int _hitCount = 0;
 
-    void OnCollisionEnter() //if ball hits board
-    {
-        //audio.Play(); //plays the hit board sound
-    }
-
-    void OnTriggerEnter() //if ball hits basket collider
-    {
-        //int currentScore = int.Parse(score.GetComponent().text) + 1; //add 1 to the score
-        //score.GetComponent().text = currentScore.ToString();
-        //AudioSource.PlayClipAtPoint(basket, transform.position); //play basket sound
-        
-        
+    void OnTriggerEnter()
+    {       
         _hitCount++;
-        //score.text = "Score: " + _hitCount;
+        score.text = "Score: " + _hitCount;
     }
 }
